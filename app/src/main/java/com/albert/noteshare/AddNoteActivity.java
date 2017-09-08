4f6f594a86d09f5d02bccde9b11fd5825877f6cf
Okwonks <albertoketch@gmail.com>
2017-09-08 17:48:58 +0300

    Adding a new activity which will hold the notes made by the user. Updating code in AddNoteActivity which will be used to get to the new activity.

--- app/src/main/java/com/albert/noteshare/AddNoteActivity.java
+++ app/src/main/java/com/albert/noteshare/AddNoteActivity.java
@@ -1,16 +1,34 @@
 package com.albert.noteshare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class AddNoteActivity extends AppCompatActivity {
     public static final String TAG = AddNoteActivity.class.getSimpleName();
+    private Button mAddNoteButton;
     private EditText mWriteEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+
+        mWriteEditText = (EditText) findViewById(R.id.noteEditText);
+        mAddNoteButton = (Button) findViewById(R.id.addNoteButton);
+
+        mAddNoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String note = mWriteEditText.getText().toString();
+                Log.d(TAG, note);
+                Intent intent = new Intent(AddNoteActivity.this, NotesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
