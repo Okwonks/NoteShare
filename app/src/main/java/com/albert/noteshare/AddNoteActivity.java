6dbc2f6a8fb9b1daada8cf1b2de8b4616b09654e
Okwonks <albertoketch@gmail.com>
2017-09-08 17:38:07 +0300

    Adding functionality to the activity_add_note.xml

--- app/src/main/java/com/albert/noteshare/AddNoteActivity.java
+++ app/src/main/java/com/albert/noteshare/AddNoteActivity.java
@@ -2,8 +2,11 @@ package com.albert.noteshare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class AddNoteActivity extends AppCompatActivity {
+    public static final String TAG = AddNoteActivity.class.getSimpleName();
+    private EditText mWriteEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
