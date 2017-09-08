d23dbbf44e725da556ac2f5771521abe97e7430b
Okwonks <albertoketch@gmail.com>
2017-09-08 17:13:36 +0300

    Adding code for the button that links the MainActivity and the AddNoteActivity.

--- app/src/main/java/com/albert/noteshare/MainActivity.java
+++ app/src/main/java/com/albert/noteshare/MainActivity.java
@@ -1,13 +1,26 @@
 package com.albert.noteshare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mMakeNoteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMakeNoteButton = (Button) findViewById(R.id.makeNoteButton);
+        mMakeNoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddNoteActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
