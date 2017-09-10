c3f43bcc587cbbdd178ae4f6bb251e14c307fd0a
Okwonks <albertoketch@gmail.com>
2017-09-10 21:12:12 +0300

    Adding an array adapter to the NotesActivity java file. This will allow for the displaying of data from a list onto the ListView.

--- app/src/main/java/com/albert/noteshare/MainActivity.java
+++ app/src/main/java/com/albert/noteshare/MainActivity.java
@@ -1,13 +1,16 @@
 package com.albert.noteshare;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button mMakeNoteButton;
+    private TextView mAppWelcomeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, AddNoteActivity.class);
                 startActivity(intent);
+
+                mAppWelcomeTextView = (TextView) findViewById(R.id.appWelcomeTextView);
+                Typeface sansationFont = Typeface.createFromAsset(getAssets(), "fonts/sansation.ttf");
+                mAppWelcomeTextView.setTypeface(sansationFont);
             }
         });
     }
--- app/src/main/java/com/albert/noteshare/NotesActivity.java
+++ app/src/main/java/com/albert/noteshare/NotesActivity.java
@@ -3,14 +3,40 @@ package com.albert.noteshare;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class NotesActivity extends AppCompatActivity {
+    private TextView mNoteTextView;
+    private ListView mNotesListView;
+    private String[] notes = new String[] {"Clean out the kitchen", "Walk the dogs", "Go for a jog", "Make some pastor"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+
+        mNotesListView = (ListView) findViewById(R.id.notesListView);
+        mNoteTextView = (TextView) findViewById(R.id.noteTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+        mNotesListView.setAdapter(adapter);
+
+        mNotesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String note = ((TextView)view).getText().toString();
+                Toast.makeText(NotesActivity.this, note, Toast.LENGTH_LONG).show();
+            }
+        });
+
+
         Intent intent = getIntent();
-        String note = intent.getStringExtra("note");
+        String newNote = intent.getStringExtra("note"); // Fix this with a TextView later on.
+        mNoteTextView.setText("Your note: " + newNote + " is being added...");
     }
 }
