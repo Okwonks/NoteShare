f91caabf1cafc15cffb65b9a726018d1c982b026
Okwonks <albertoketch@gmail.com>
2017-09-15 12:45:27 +0300

    Adding the Twitter API to the project. This will allow the users to search through Twitter as they make their notes.

---
+++ app/src/main/java/com/albert/noteshare/Constants.java
@@ -0,0 +1,14 @@
+package com.albert.noteshare;
+
+/**
+ * Created by albert on 9/15/17.
+ */
+
+public class Constants {
+    public static final String TWITTER_CONSUMER_KEY = BuildConfig.TWITTER_CONSUMER_KEY;
+    public static final String TWITTER_CONSUMER_SECRET = BuildConfig.TWITTER_CONSUMER_SECRET;
+    public static final String TWITTER_ACCESS_TOKEN = BuildConfig.TWITTER_ACCESS_TOKEN;
+    public static final String TWITTER_TOKEN_SECRET = BuildConfig.TWITTER_TOKEN_SECRET;
+    public static final String BASE_URL = "https://api.twitter.com/1.1/search/tweets.json";
+    public static final String TWITTER_TWEET_QUERY_PARAMETER = "";
+}
---
+++ app/src/main/java/com/albert/noteshare/services/TwitterService.java
@@ -0,0 +1,8 @@
+package com.albert.noteshare.services;
+
+/**
+ * Created by albert on 9/15/17.
+ */
+
+public class TwitterService {
+}
