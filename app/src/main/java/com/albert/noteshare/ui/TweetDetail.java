90d06c66fe5945fafa94ce74afedb8c269c3368f
Okwonks <albertoketch@gmail.com>
2017-09-18 16:36:35 +0300

    New activity that will show details about tweets.

---
+++ app/src/main/java/com/albert/noteshare/ui/TweetDetail.java
@@ -0,0 +1,15 @@
+package com.albert.noteshare.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.albert.noteshare.R;
+
+public class TweetDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tweet_detail);
+    }
+}
