be109a27d90853612526cf91ab53c546fd652cfb
Okwonks <albertoketch@gmail.com>
2017-09-21 17:08:32 +0300

    New activity for users to write their own notes. The activity will also allow the notes to be saved.

--- app/src/main/java/com/albert/noteshare/ui/WriteNoteActivity.java
+++ app/src/main/java/com/albert/noteshare/ui/WriteNoteActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import com.albert.noteshare.R;
 
-public class TweetDetail extends AppCompatActivity {
+public class WriteNoteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tweet_detail);
+        setContentView(R.layout.activity_write_note);
     }
 }
