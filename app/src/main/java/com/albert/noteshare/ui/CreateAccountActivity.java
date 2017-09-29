cd4ef3d1e477295bc33b465199a0d9ce4a99e10c
Okwonks <albertoketch@gmail.com>
2017-09-29 15:29:36 +0300

    Constraints to the sign up fields to ensure users add the correct fields to requested fields. Helps in setting up authentication for the app.

--- app/src/main/java/com/albert/noteshare/ui/CreateAccountActivity.java
+++ app/src/main/java/com/albert/noteshare/ui/CreateAccountActivity.java
@@ -2,18 +2,22 @@ package com.albert.noteshare.ui;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.albert.noteshare.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -27,6 +31,7 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     @Bind(R.id.loginTextView) TextView mLoginTextView;
 
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,8 +62,69 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
 
+                } else {
+                    Toast.makeText(CreateAccountActivity.this, "Authentication Failed!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+
+    private void createAuthListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private boolean validEmail(String email) {
+        boolean goodEmail = (email != null && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+        if (!goodEmail) {
+            mEmailEditText.setError("Please enter a valid email address!");
+            return false;
+        }
+        return goodEmail;
+    }
+
+    private boolean validName(String name) {
+        if (name.equals("")) {
+            mNameEditText.setError("Please enter your name!");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validPassword(String password, String confirmPassword) {
+        if (password.length() < 6) {
+            mPasswordEditText.setError("Enter a password not less than 6 characters!");
+            return false;
+        } else if (!password.equals(confirmPassword)) {
+            mPasswordEditText.setError("Password does not match");
+            return false;
+        }
+        return true;
     }
 }
